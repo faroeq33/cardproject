930bfa6920c9db8bc049cbf95f05f8decf4cccbb
Faroeq <alves_1044@hotmail.com>
2024-08-05 20:31:42 +0200

    configure darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import ProductCard from "./components/ProductCard";
+import { ThemeProvider } from "@/components/theme-provider";
 
 function App() {
   return (
     <>
       <main className="flex items-center justify-center h-screen p-3">
         <div className="cardwrapper max-w-[600px]">
-          <ProductCard />
+          <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <ProductCard />
+          </ThemeProvider>
         </div>
       </main>
     </>
